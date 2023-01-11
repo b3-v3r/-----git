58205b273b78ce0c35f769a6b77479c6d9e8f8fd
Serhii Silchuk <sergeyds2345@gmail.com>
2023-01-11 20:09:46 +0200

    second commit

---
+++ Классы2.py
@@ -0,0 +1,37 @@
+class Container :
+	def __init__(self , name :str , volume : int , current_volume : int , pour_out : int = 100 ) -> None:
+		self.__name = name 
+		self.__volume = volume
+		self.current_volume = current_volume
+		self.pour_out = pour_out
+	
+	def pour_out_liqud(self) -> int :
+		if self.current_volume > 0 :
+			self.current_volume -= self.pour_out
+			return self.pour_out
+		else:
+			print(f'{self.__name} я пустой')
+			return 0
+
+	def pour_liqud(self , volume : int ) -> None :
+		if self.current_volume + volume <= self.__volume :
+			self.current_volume += volume
+		elif self.current_volume >= self.__volume :
+			print(f'{self.__name} я полный')
+	
+	def info(self):
+		print(f'{self.__name} = {self.current_volume}')
+
+def main():
+	chainik = Container(name = 'chainik', volume = 700 , current_volume = 700   )
+	krychka = Container(name = 'Krychka', volume = 200, current_volume = 0 , pour_out = 50  )
+	i = 0
+	while i < 10 :
+		chainik.info()
+		krychka.info()
+		krychka.pour_liqud(chainik.pour_out_liqud())
+		i += 1
+
+
+if __name__ == '__main__':
+	main()
